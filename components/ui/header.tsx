7b583604bc3a56a3dca745a245aaa3ed678ac613
kimyziii <yeaji105@naver.com>
2023-10-02 01:33:21 +0900

    헤더 화면 생성

---
+++ components/ui/header.tsx
@@ -0,0 +1,81 @@
+export default function Header() {
+  return (
+    <div className="header">
+      {/* 로고 / 검색 컴포넌트 / 프로필 */}
+
+      {/* 로고 */}
+      <div className="presentation" role="presentation">
+        <img
+          className="logo"
+          src="/images/sales-support-logo.png"
+          alt="salesSupportLogo"
+        />
+      </div>
+
+      {/* 검색 */}
+      <div className="searchWrapper">
+        <input className="searchInput" />
+        <img className="searchIcon" src="/icons/search.svg" />
+      </div>
+
+      {/* 프로필 */}
+      <div className="presentation" role="presentation">
+        <img className="image" src="/icons/profile.svg" alt="profileLogo" />
+      </div>
+
+      <style jsx>{`
+        img {
+          background-color: white;
+        }
+
+        .header {
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          margin: 10px 15px;
+        }
+
+        .presentation {
+          cursor: pointer;
+          display: flex;
+          align-items: center;
+        }
+
+        .searchWrapper {
+          display: flex;
+          align-items: center;
+          border: 1px solid #666666;
+          border-radius: 7px;
+          width: calc((100vw - 150px) * 0.75);
+          height: 30px;
+          padding: 0 5px;
+          gap: 8px;
+        }
+
+        .searchInput {
+          width: calc(100% - 23px);
+          border: none;
+          padding: 0 10px;
+        }
+
+        .searchInput:focus,
+        .searchInput:hover {
+          outline: none;
+        }
+
+        .searchIcon {
+          width: 23px;
+          cursor: pointer;
+        }
+
+        .logo {
+          width: 150px;
+        }
+
+        .image {
+          width: 30px;
+        }
+      `}</style>
+    </div>
+  );
+}
