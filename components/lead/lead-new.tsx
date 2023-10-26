ef3efe862bdb252edeb11fb01a2aa5ac7d9da167
kimyziii <yeaji105@naver.com>
2023-10-26 19:06:04 +0900

    Feat: lead read and create

--- components/lead/lead-new.tsx
+++ components/lead/lead-new.tsx
@@ -1,209 +1,227 @@
-import React, { useState } from "react";
+import axios from 'axios'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { LeadProps } from '../ui/list-view'
 
 interface FormData {
-  contactName: string;
-  title?: string;
-  email: string;
-  phone: string;
-  companyName: string;
-  industry?: string;
-  employeeNumber?: number;
-  leadSource: string;
-  leadStatus: string;
-  productInterest?: string;
-  description?: string;
+  contactName: string
+  title?: string
+  email: string
+  phone: string
+  companyName: string
+  industry?: string
+  employeeNumber?: number
+  leadSource: string
+  leadStatus: string
+  productInterest?: string
+  description?: string
+  platform: string
+  createdAt: string
 }
 
 type NewLeadProps = {
-  closeModal: () => void;
-};
+  closeModal: () => void
+  submitForm: (formData: LeadProps) => void
+}
 
-const NewLead: React.FC<NewLeadProps> = ({ closeModal }) => {
+export function phoneFormatter(phoneNumber: string) {
+  if (phoneNumber.length === 11) {
+    return (
+      phoneNumber.slice(0, 3) +
+      '-' +
+      phoneNumber.slice(3, 7) +
+      '-' +
+      phoneNumber.slice(7)
+    )
+  }
+  return phoneNumber
+}
+
+const NewLead: React.FC<NewLeadProps> = ({ closeModal, submitForm }) => {
   const initialFormData: FormData = {
-    contactName: "",
-    title: "",
-    email: "",
-    phone: "",
-    companyName: "",
-    industry: "",
+    contactName: '',
+    title: '',
+    email: '',
+    phone: '',
+    companyName: '',
+    industry: '',
     employeeNumber: 0,
-    leadSource: "",
-    leadStatus: "열림",
-    productInterest: "",
-    description: "",
-  };
-
-  const [formData, setFormData] = useState<FormData>(initialFormData);
-  function submitData(event: React.MouseEvent<HTMLButtonElement>) {
-    console.log(formData);
+    leadSource: '',
+    leadStatus: '열림',
+    productInterest: '',
+    description: '',
+    platform: 'SalesSupport',
+    createdAt: '',
   }
 
+  const [formData, setFormData] = useState<FormData>(initialFormData)
+
   function selectChangeStatus(event: React.ChangeEvent<HTMLSelectElement>) {
-    const { name, value } = event.target;
+    const { name, value } = event.target
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    }));
+    }))
   }
 
   function inputChangeHandler(event: React.ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
+    const { name, value } = event.target
+    if (name === 'phone') phoneFormatter(value)
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    }));
+    }))
   }
 
   function textAreaChangeHandler(
-    event: React.ChangeEvent<HTMLTextAreaElement>
+    event: React.ChangeEvent<HTMLTextAreaElement>,
   ) {
-    const { name, value } = event.target;
+    const { name, value } = event.target
     setFormData((prevData) => ({
       ...prevData,
       [name]: value,
-    }));
+    }))
   }
 
   return (
-    <div className="body">
-      <header className="header">새로운 리드</header>
-      <div className="section">
+    <div className='body'>
+      <header className='header'>새로운 리드</header>
+      <div className='section'>
         <h3>담당자 정보</h3>
-        <div className="section-row">
-          <div className="input-wrapper">
-            <label htmlFor="contactName">이름</label>
+        <div className='section-row'>
+          <div className='input-wrapper'>
+            <label htmlFor='contactName'>이름</label>
             <input
-              type="text"
-              id="contactName"
-              name="contactName"
+              type='text'
+              id='contactName'
+              name='contactName'
               onChange={inputChangeHandler}
             />
           </div>
-          <div className="input-wrapper">
-            <label htmlFor="title">직급</label>
+          <div className='input-wrapper'>
+            <label htmlFor='title'>직급</label>
             <input
-              type="text"
-              id="title"
-              name="title"
+              type='text'
+              id='title'
+              name='title'
               onChange={inputChangeHandler}
             />
           </div>
         </div>
-        <div className="section-row">
-          <div className="input-wrapper">
-            <label htmlFor="email">이메일</label>
+        <div className='section-row'>
+          <div className='input-wrapper'>
+            <label htmlFor='email'>이메일</label>
             <input
-              type="text"
-              id="email"
-              name="email"
+              type='text'
+              id='email'
+              name='email'
               onChange={inputChangeHandler}
             />
           </div>
-          <div className="input-wrapper">
-            <label htmlFor="phone">전화번호</label>
+          <div className='input-wrapper'>
+            <label htmlFor='phone'>전화번호</label>
             <input
-              type="text"
-              id="phone"
-              name="phone"
+              type='text'
+              id='phone'
+              name='phone'
               onChange={inputChangeHandler}
             />
           </div>
         </div>
       </div>
-      <div className="section">
+      <div className='section'>
         <h3>회사 정보</h3>
-        <div className="section-row">
-          <div className="input-wrapper">
-            <label htmlFor="companyName">회사명</label>
+        <div className='section-row'>
+          <div className='input-wrapper'>
+            <label htmlFor='companyName'>회사명</label>
             <input
-              type="text"
-              id="companyName"
-              name="companyName"
+              type='text'
+              id='companyName'
+              name='companyName'
               onChange={inputChangeHandler}
             />
           </div>
-          <div className="input-wrapper">
-            <label htmlFor="industry">산업군</label>
+          <div className='input-wrapper'>
+            <label htmlFor='industry'>산업군</label>
             <input
-              type="text"
-              id="industry"
-              name="industry"
+              type='text'
+              id='industry'
+              name='industry'
               onChange={inputChangeHandler}
             />
           </div>
         </div>
-        <div className="section-row">
-          <div className="input-wrapper">
-            <label htmlFor="employeeNumber">사원수</label>
+        <div className='section-row'>
+          <div className='input-wrapper'>
+            <label htmlFor='employeeNumber'>사원수</label>
             <input
-              type="number"
-              id="employeeNumber"
-              name="employeeNumber"
+              type='number'
+              id='employeeNumber'
+              name='employeeNumber'
               onChange={inputChangeHandler}
             />
           </div>
-          <div className="input-wrapper"></div>
+          <div className='input-wrapper'></div>
         </div>
       </div>
-      <div className="section">
+      <div className='section'>
         <h3>리드 정보</h3>
-        <div className="section-row">
-          <div className="input-wrapper">
-            <label htmlFor="leadSource">리드 소스</label>
+        <div className='section-row'>
+          <div className='input-wrapper'>
+            <label htmlFor='leadSource'>리드 소스</label>
             <input
-              type="text"
-              id="leadSource"
-              name="leadSource"
+              type='text'
+              id='leadSource'
+              name='leadSource'
               onChange={inputChangeHandler}
             />
           </div>
-          <div className="input-wrapper">
-            <label htmlFor="leadStatus">리드 상태</label>
-            <select id="leadStatus" onChange={selectChangeStatus}>
-              <option value="open">열림</option>
-              <option value="working">진행중</option>
-              <option value="closed">닫힘</option>
-              <option value="converted">전환됨</option>
+          <div className='input-wrapper'>
+            <label htmlFor='leadStatus'>리드 상태</label>
+            <select id='leadStatus' onChange={selectChangeStatus}>
+              <option value='open'>열림</option>
+              <option value='working'>진행중</option>
+              <option value='closed'>닫힘</option>
+              <option value='converted'>전환됨</option>
             </select>
           </div>
         </div>
-        <div className="section-row">
-          <div className="input-wrapper">
-            <label htmlFor="productInterest">관심 상품</label>
+        <div className='section-row'>
+          <div className='input-wrapper'>
+            <label htmlFor='productInterest'>관심 상품</label>
             <input
-              type="text"
-              id="productInterest"
-              name="productInterest"
+              type='text'
+              id='productInterest'
+              name='productInterest'
               onChange={inputChangeHandler}
             />
           </div>
-          <div className="input-wrapper"></div>
+          <div className='input-wrapper'></div>
         </div>
       </div>
-      <div className="section">
+      <div className='section'>
         <h3>기타 정보</h3>
-        <div className="textarea-wrapper">
-          <label className="textarea-label" htmlFor="description">
+        <div className='textarea-wrapper'>
+          <label className='textarea-label' htmlFor='description'>
             설명
           </label>
           <textarea
-            className="textarea"
-            id="description"
-            name="description"
+            className='textarea'
+            id='description'
+            name='description'
             onChange={textAreaChangeHandler}
           />
         </div>
       </div>
 
-      <div className="footer">
+      <div className='footer'>
         <button
-          type="submit"
-          className="footer-btn footer-btn-save"
-          onClick={submitData}
+          className='footer-btn footer-btn-save'
+          onClick={() => submitForm(formData)}
         >
           저장
         </button>
-        <button className="footer-btn footer-btn-cancel" onClick={closeModal}>
+        <button className='footer-btn footer-btn-cancel' onClick={closeModal}>
           취소
         </button>
       </div>
@@ -318,7 +336,7 @@ const NewLead: React.FC<NewLeadProps> = ({ closeModal }) => {
         }
       `}</style>
     </div>
-  );
-};
+  )
+}
 
-export default NewLead;
+export default NewLead
--- components/ui/list-view.tsx
+++ components/ui/list-view.tsx
@@ -1,59 +1,176 @@
-import React from "react";
-import Modal from "./modal/modal";
-import NewLead from "../lead/lead-new";
+import Modal from './modal/modal'
+import NewLead, { phoneFormatter } from '../lead/lead-new'
 
-import { useEffect, useState } from "react";
-import { db } from "@/firebaseApp";
-import { collection, doc, getDocs } from "firebase/firestore";
+import { useEffect, useState } from 'react'
+import { addDoc, collection, getDocs, orderBy, query } from 'firebase/firestore'
+import { db } from '../../firebaseApp'
+import Link from 'next/link'
 
 interface ListViewProps {
-  pathname: string;
+  pathname: string
+}
+
+export interface LeadProps {
+  id?: string
+  contactName: string
+  title?: string
+  email: string
+  phone: string
+  companyName: string
+  industry?: string
+  employeeNumber?: number
+  leadSource: string
+  leadStatus: string
+  productInterest?: string
+  description?: string
+  platform: string
+  createdAt: string
+}
+
+function formatCurrentTime() {
+  const now = new Date()
+
+  const year = now.getFullYear()
+  const month = now.getMonth() + 1
+  const day = now.getDate()
+
+  let hours: string | number = now.getHours()
+  const minutes = now.getMinutes()
+
+  let amOrPm = '오전'
+  if (hours >= 12) {
+    amOrPm = '오후'
+    if (hours > 12) {
+      hours -= 12
+    }
+  }
+
+  hours = hours.toString().padStart(2, '0')
+
+  const formattedMonth = String(month).padStart(2, '0')
+  const formattedDay = String(day).padStart(2, '0')
+  const formattedMinutes = String(minutes).padStart(2, '0')
+
+  const formattedTime = `${year}년 ${formattedMonth}월 ${formattedDay}일 ${amOrPm} ${hours}:${formattedMinutes}`
+
+  return formattedTime
 }
 
 export default function ListView({ pathname }: ListViewProps) {
-  const [name, setName] = useState("");
-  const [isOpen, setIsOpen] = useState(false);
-  const [data, setData] = useState(null);
-  const [nullData, setNullData] = useState(false);
+  const [name, setName] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+  const [data, setData] = useState<LeadProps[]>([])
 
   useEffect(() => {
-    if (pathname === "/") setName("홈");
-    if (pathname === "/lead") setName("리드");
-    if (pathname === "/account") setName("고객");
-    if (pathname === "/contact") setName("연락처");
-    if (pathname === "/opportunity") setName("기회");
-  }, [pathname]);
+    if (pathname === '/') setName('홈')
+    if (pathname === '/lead') setName('리드')
+    if (pathname === '/account') setName('고객')
+    if (pathname === '/contact') setName('연락처')
+    if (pathname === '/opportunity') setName('기회')
+  }, [pathname])
+
+  async function getLeads() {
+    setData([])
+
+    let postsRef = collection(db, 'lead')
+    const queryRef = query(postsRef, orderBy('createdAt'))
+    const datas = await getDocs(queryRef)
+
+    datas?.forEach((doc) => {
+      const dataObj = {
+        ...doc.data(),
+        phone: phoneFormatter(doc.data().phone),
+        id: doc.id,
+      }
+      setData((prev) => [...prev, dataObj as LeadProps])
+    })
+  }
+
+  useEffect(() => {
+    getLeads()
+  }, [])
+
+  async function handleSubmitForm(formData: LeadProps) {
+    const data = { ...formData, createdAt: formatCurrentTime() }
+    await addDoc(collection(db, 'lead'), data)
+      .then(() => handleCloseModal())
+      .then(() => getLeads())
+  }
 
   function handleOpenModal() {
-    setIsOpen(true);
+    setIsOpen(true)
   }
 
   function handleCloseModal() {
-    setIsOpen(false);
+    setIsOpen(false)
   }
-
   return (
     <>
       {isOpen && (
         <Modal closeModal={handleCloseModal}>
-          {pathname === "/lead" && <NewLead closeModal={handleCloseModal} />}
+          {pathname === '/lead' && (
+            <NewLead
+              closeModal={handleCloseModal}
+              submitForm={(formData) => {
+                handleSubmitForm(formData)
+              }}
+            />
+          )}
         </Modal>
       )}
-      <div className="list-view">
-        <div className="list-view__header">
-          <button className="list-view__button--change">
+      <div className='list-view'>
+        <div className='list-view__header'>
+          <button className='list-view__button--change'>
             <div>모든 {name}</div>
-            <img src="/icons/chevron-down.svg" style={{ width: "15px" }} />
+            <img src='/icons/chevron-down.svg' style={{ width: '15px' }} />
           </button>
-          <button className="list-view__button--new" onClick={handleOpenModal}>
+          <button className='list-view__button--new' onClick={handleOpenModal}>
             새로 만들기
           </button>
         </div>
 
-        {/* data list view */}
-        {nullData && <p>데이터가 없습니다.</p>}
-
+        {data?.length === 0 ? (
+          <p>데이터가 없습니다.</p>
+        ) : (
+          <table className='rwd-table'>
+            <tbody>
+              <tr>
+                <th>Title</th>
+                <th>Contact Name</th>
+                <th>Company Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Lead Status</th>
+                <th>Created At</th>
+              </tr>
+              {data?.map((lead) => (
+                <tr key={`${lead.id}`}>
+                  <td data-th='Title'>
+                    <Link
+                      href={`/lead/${lead?.id}`}
+                      style={{
+                        textDecoration: 'none',
+                      }}
+                    >
+                      {lead.title}
+                    </Link>
+                  </td>
+                  <td data-th='Contact Name'>{lead.contactName}</td>
+                  <td data-th='Company Name'>{lead.companyName}</td>
+                  <td data-th='Email'>{lead.email}</td>
+                  <td data-th='Phone'>{lead.phone}</td>
+                  <td data-th='Lead Status'>{lead.leadStatus}</td>
+                  <td data-th='Created At'>{lead.createdAt}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
         <style jsx>{`
+          a {
+            text-decoration: none;
+          }
+
           .list-view {
             width: 100%;
             border: 1px solid #dedede;
@@ -72,6 +189,7 @@ export default function ListView({ pathname }: ListViewProps) {
             flex-direction: row;
             align-items: center;
             justify-content: space-between;
+            margin-bottom: 12px;
           }
 
           .list-view__button--change {
@@ -94,8 +212,107 @@ export default function ListView({ pathname }: ListViewProps) {
             border-radius: 7px;
             cursor: pointer;
           }
+
+          .table-data {
+            text-align: center;
+          }
+
+          .rwd-table {
+            min-width: 300px;
+            max-width: 100%;
+            border-collapse: collapse;
+            font-size: 12px;
+            overflow: scroll-x;
+          }
+
+          .rwd-table tr:first-child {
+            border-top: none;
+            background: black;
+            color: #fff;
+          }
+
+          .rwd-table tr {
+            border-top: 1px solid #ddd;
+            border-bottom: 1px solid #ddd;
+            background-color: #ffffff;
+          }
+
+          .rwd-table th {
+            display: none;
+          }
+
+          .rwd-table td {
+            display: block;
+          }
+
+          .rwd-table td:first-child {
+            margin-top: 0.5em;
+          }
+
+          .rwd-table td:last-child {
+            margin-bottom: 0.5em;
+          }
+
+          .rwd-table td:before {
+            content: attr(data-th) ': ';
+            font-weight: bold;
+            width: 120px;
+            display: inline-block;
+            color: #000;
+          }
+
+          .rwd-table th,
+          .rwd-table td {
+            text-align: left;
+          }
+
+          .rwd-table {
+            color: #333;
+            border-radius: 0.4em;
+            overflow: hidden;
+          }
+
+          .rwd-table tr {
+            border-color: #bfbfbf;
+          }
+
+          .rwd-table th,
+          .rwd-table td {
+            padding: 0.5em 1em;
+          }
+
+          @media screen and (max-width: 601px) {
+            .rwd-table tr:nth-child(2) {
+              border-top: none;
+            }
+          }
+          @media screen and (min-width: 600px) {
+            .rwd-table tr:hover:not(:first-child) {
+              background-color: #d8e7f3;
+            }
+            .rwd-table td:before {
+              display: none;
+            }
+            .rwd-table th,
+            .rwd-table td {
+              display: table-cell;
+              padding: 0.25em 0.5em;
+            }
+            .rwd-table th:first-child,
+            .rwd-table td:first-child {
+              padding-left: 0;
+            }
+            .rwd-table th:last-child,
+            .rwd-table td:last-child {
+              padding-right: 0;
+            }
+            .rwd-table th,
+            .rwd-table td {
+              padding: 1em !important;
+            }
+          }
         `}</style>
       </div>
     </>
-  );
+  )
 }
