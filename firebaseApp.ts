abed6f1c2e2aa88f6848ca4e37e70a146ee68a1a
kimyziii <yeaji105@naver.com>
2023-10-26 19:04:18 +0900

    firebase config

---
+++ firebaseApp.ts
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, FirebaseApp, getApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+}
+
+export let app: FirebaseApp
+
+try {
+  app = getApp('app')
+} catch (e) {
+  app = initializeApp(firebaseConfig, 'app')
+}
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig)
+
+export const db = getFirestore(app)
+export default firebase
