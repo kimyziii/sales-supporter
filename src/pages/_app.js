217dd2851e9c0b7c1174ba5bf73be402547e9a3a
kimyziii <yeaji105@naver.com>
2023-11-30 22:05:22 +0900

    토스트 메세지 자동 닫기 시간 수정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,7 +7,7 @@ import 'react-toastify/dist/ReactToastify.css'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <ToastContainer />
+      <ToastContainer autoClose={2000} />
       <Layout>
         <Component {...pageProps} />
       </Layout>
