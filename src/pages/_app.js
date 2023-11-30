5c17dc1befff382c331ea28e014be413e83d96f8
kimyziii <yeaji105@naver.com>
2023-11-30 16:37:52 +0900

    toastify 설정

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import Layout from '@/components/ui/Layout'
 import '@/styles/globals.css'
 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <ToastContainer />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
